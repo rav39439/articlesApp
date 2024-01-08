61064664643067a5d6424bf56a81d5143c4bc146
Ritesh <riteshcg91@gmail.com>
2024-01-08 12:37:58 +0530

    changes to UI

--- src/app/port-folio-user/port-folio-user.component.ts
+++ src/app/port-folio-user/port-folio-user.component.ts
@@ -10,6 +10,8 @@ email='rav39439@gmail.com'
 isDropdownActive = false;
 isDropdownActive1 = false;
 isDropdownActive2 = false;
+isDropdownActive3 = false;
+isDropdownActive4 = false;
 
 toggleList() {
 //  let ds=document.getElementById('hhjjdd') as HTMLElement
@@ -51,26 +53,55 @@ toggleList1() {
 }
 
 toggleList2() {
- // let ds=document.getElementById('hhjjdd') as HTMLElement
-    let ds2=document.getElementById('mmmm') as HTMLElement
+    let ds2=document.getElementById('hhjjdd3') as HTMLElement
 
    if(!this.isDropdownActive2){
-   // alert("ddd")
-   // ds.classList.add('createSpace')
-  //  ds2.style.paddingTop='1200px'
+
     ds2.classList.add('createSpace2')
-  //  ds2.classList.add('createSpace')
 
   }
    else{
-    // ds2.style.paddingTop='0px'
 
-    ///ds.classList.remove('createSpace')
     ds2.classList.remove('createSpace2')
-   // ds2.classList.remove('createSpace')
 
    }
   this.isDropdownActive2 = !this.isDropdownActive2;
+
+
+}
+
+toggleList3() {
+  let ds2=document.getElementById('hhjjdd4') as HTMLElement
+
+ if(!this.isDropdownActive3){
+
+  ds2.classList.add('createSpace4')
+
+}
+ else{
+
+  ds2.classList.remove('createSpace4')
+
+ }
+this.isDropdownActive3 = !this.isDropdownActive3;
+
+
+}
+
+toggleList4() {
+  let ds2=document.getElementById('mmmm') as HTMLElement
+
+ if(!this.isDropdownActive4){
+
+  ds2.classList.add('createSpace3')
+
+}
+ else{
+
+  ds2.classList.remove('createSpace3')
+
+ }
+this.isDropdownActive4 = !this.isDropdownActive4;
 
 
 }
